9f66919b12888bda6d9be10affc08481cec8d243
bigh0124 <thubgi0629@gmail.com>
2024-08-08 20:52:36 +0800

    Feat: Add Navbar boilerplate and link to  DashboardWrapper

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "@/app/(components)/Navbar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       SideBar
-      <main className={`flex flex-col w-full h-full bg-gray-50 px-9 py-7 md:pl-24`}>{children}</main>
+      <main className={`flex flex-col w-full h-full bg-gray-50 px-9 py-7 md:pl-24`}>
+        <Navbar />
+        {children}
+      </main>
     </div>
   );
 };
