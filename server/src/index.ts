6e3c2fcd58df32c75103eec1b7bdeafbcd57da29
bigh0124 <thubgi0629@gmail.com>
2024-08-15 15:53:14 +0800

    Chore: Init express server for backend

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+// ROUTE IMPORTS
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("common"));
+
+// ROUTES
+
+// SERVER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
