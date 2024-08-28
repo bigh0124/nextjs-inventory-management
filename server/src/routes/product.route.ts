356ace6bcc707eec93ac6e923f9f9d36e8d57600
bigh0124 <thubgi0629@gmail.com>
2024-08-28 14:29:34 +0800

    Feat: Add create product route

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/product.controller";
+import { createProduct, getProducts } from "../controllers/product.controller";
 
 const router = Router();
 
 router.get("/", getProducts);
+router.post("/", createProduct);
 
 export default router;
