85faf9610aa8d71747f2ba5350dd6858fb5888d1
bigh0124 <thubgi0629@gmail.com>
2024-09-06 15:14:04 +0800

    Chore: Add config file for aws ec2

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,7 +27,7 @@ app.use("/users", userRoute);
 app.use("/expenses", expenseRoute);
 
 // SERVER
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
